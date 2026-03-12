82dd07aa525a416519b106e87763c9b8e302c38d
Malakai Ricketts <codedbymalakai@gmail.com>
2026-03-12 00:32:05 +0000

    feat: add imports and /companies/search/:postcode endpoint

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,10 +1,35 @@
 import express from "express";
-import { searchCompanies } from "../services/companiesHouseService.js";
+import {
+  searchCompanies,
+  getCompanyDetails,
+} from "../services/companiesHouseService.js";
 
 const router = express.Router();
 
 router.get("/company/:companyNumber", (req, res) => {
   // Call function from services
+});
+
+router.get("/companies/search/:postcode", async (req, res) => {
+  const { postcode } = req.params;
+  if (!postcode) {
+    return res.status(400).json({
+      ok: false,
+      error: "Postcode is required for searching",
+    });
+  }
+  try {
+    const companies = await searchCompanies(postcode);
+    return res.status(200).json({
+      ok: true,
+      data: companies,
+    });
+  } catch (error) {
+    return res.status(error?.statusCode || 500).json({
+      ok: false,
+      error: error.message || "Unknown error",
+    });
+  }
 });
 
 export default router;
