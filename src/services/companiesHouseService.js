0993fa9723e2bfe9b16e7422089a4d8d95eb1983
Malakai Ricketts <codedbymalakai@gmail.com>
2026-03-12 16:28:17 +0000

    feat: add compareCompanies functionality

--- src/services/companiesHouseService.js
+++ src/services/companiesHouseService.js
@@ -58,3 +58,49 @@ export async function searchCompanies(postcode) {
     throw newError;
   }
 }
+
+export async function compareCompanies(postcode, allowedCompanies) {
+  if (
+    !postcode ||
+    !allowedCompanies ||
+    !Array.isArray(allowedCompanies) ||
+    allowedCompanies.length === 0
+  ) {
+    const error = new Error(
+      "Postcode is required and allowedCompanies must be a non-empty array.",
+    );
+    error.statusCode = 400;
+    throw error;
+  }
+  const allowed = [];
+  const unauthorised = [];
+  const allowedCompaniesSet = new Set(allowedCompanies);
+  try {
+    const companies = await searchCompanies(postcode);
+    companies.forEach((company) => {
+      if (allowedCompaniesSet.has(company.companyNumber)) {
+        allowed.push({
+          companyNumber: company.companyNumber,
+          title: company.companyName,
+        });
+      } else {
+        unauthorised.push({
+          companyNumber: company.companyNumber,
+          title: company.companyName,
+        });
+      }
+    });
+    return {
+      allowed,
+      unauthorised,
+    };
+  } catch (error) {
+    const newError = new Error(
+      error?.response?.data?.error ||
+        error.message ||
+        "Failed to compare companies",
+    );
+    newError.statusCode = error?.response?.status || 500;
+    throw newError;
+  }
+}
